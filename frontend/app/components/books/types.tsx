WardenPro/Librario-B3

Remove the BooksClient component and reorganize the structure of the book pages. Add new components for book management, including the book list, book details, copies, and reviews. Implement a dialog for adding books and improve reservation management.

frontend/app/components/books/types.tsx
@@ -1,0 +1,53 @@
+export type Book = {
+    id: number;
+    title: string;
+    author: string;
+    ISBN_10: string | null;
+    ISBN_13: string | null;
+    description: string;
+    printType: string;
+    category: string;
+    publisher: string;
+    quantity: number;
+    language: string;
+    pageCount: number;
+    publish_date: string;
+    image_link: string | null;
+  };
+  
+  export type Copy = {
+    copy_id: number;
+    state: string;
+    is_reserved: boolean;
+    is_claimed: boolean;
+    book_id: number;
+    review_condition: string[] | null;
+  };
+  
+  export type Review = {
+    id: number;
+    book_id: number;
+    user_id: number;
+    description: string;
+    note: number;
+    user?: {
+      first_name: string;
+      last_name: string;
+    };
+  };
+  
+  export type Pagination = {
+    total: number;
+    page: number;
+    itemsPerPage: number;
+    totalPages: number;
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+  };
+  
+  export type User = {
+    id: number;
+    email: string;
+    first_name?: string;
+    last_name?: string;
+  };

Biblio-B3/Biblio-B3

frontend/app/components/books/types.tsx
@@ -1,54 +1,54 @@
 export type Book = {
-    id: number;
-    title: string;
-    author: string;
-    ISBN_10: string | null;
-    ISBN_13: string | null;
-    description: string;
-    printType: string;
-    category: string;
-    publisher: string;
-    quantity: number;
-    language: string;
-    pageCount: number;
-    publish_date: string;
-    image_link: string | null;
-  };
-  
-  export type Copy = {
-    copy_id: number;
-    state: string;
-    is_reserved: boolean;
-    is_claimed: boolean;
-    book_id: number;
-    review_condition: string[] | null;
-  };
-  
-  export type Review = {
-    id: number;
-    description: string;
-    note: number;
-    condition: number;
-    copy_id: number;
-    book_id: number;
-    user_id: number;
-    user_first_name: string;
-    user_last_name: string;
-    book_title: string;
-  };
-  
-  export type Pagination = {
-    total: number;
-    page: number;
-    itemsPerPage: number;
-    totalPages: number;
-    hasNextPage: boolean;
-    hasPreviousPage: boolean;
-  };
-  
-  export type User = {
-    id: number;
-    email: string;
-    first_name?: string;
-    last_name?: string;
-  };
+  id: number;
+  title: string;
+  author: string;
+  ISBN_10: string | null;
+  ISBN_13: string | null;
+  description: string;
+  printType: string;
+  category: string;
+  publisher: string;
+  quantity: number;
+  language: string;
+  pageCount: number;
+  publish_date: string;
+  image_link: string | null;
+};
+
+export type Copy = {
+  copy_id: number;
+  state: string;
+  is_reserved: boolean;
+  is_claimed: boolean;
+  book_id: number;
+  review_condition: string[] | null;
+};
+
+export type Review = {
+  id: number;
+  description: string;
+  note: number;
+  condition: number;
+  copy_id: number;
+  book_id: number;
+  user_id: number;
+  user_first_name: string;
+  user_last_name: string;
+  book_title: string;
+};
+
+export type Pagination = {
+  total: number;
+  page: number;
+  itemsPerPage: number;
+  totalPages: number;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+};
+
+export type User = {
+  id: number;
+  email: string;
+  first_name?: string;
+  last_name?: string;
+};