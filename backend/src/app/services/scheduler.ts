1634255635669631a8cf618893223e5de9b348e6
Warden Pro <warmax7794@gmail.com>
2025-06-12 15:34:42 +0200

    feat: remove unclaimed reservation reminder from scheduler

--- backend/src/app/services/scheduler.ts
+++ backend/src/app/services/scheduler.ts
@@ -1,8 +1,6 @@
 import cron from "node-cron";
 import "./expired_reservation";
-import "./unclaimed_reservation_reminder";
 import "./reservation_expiration_reminder";
-import { unclaimed_reservation_reminder } from "./unclaimed_reservation_reminder";
 import { expired_reservation } from "./expired_reservation";
 import { reservation_expiration_reminder } from "./reservation_expiration_reminder";
 import { logMessage } from "../utils/logger";
@@ -14,8 +12,6 @@ export function startScheduler() {
     });
 
     cron.schedule("0 6-23/3 * * *", async () => {
-        logMessage("Executing the 'unclaimed_reservation_reminder' task.");
-        await unclaimed_reservation_reminder();
         logMessage("Executing the 'reservation_expiration_reminder' task.");
         await reservation_expiration_reminder();
     });
